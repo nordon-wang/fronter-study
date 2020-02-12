26cfd7f11aca6823dc00d5d1f29e8350dddd7ae4
nordon.wang <nordon.wang@oyohotels.cn>
2020-02-12 23:32:10 +0800

    第一天的学习笔记

---
+++ src/zf/01promise/demo1.js
@@ -0,0 +1,131 @@
+/** 
+ * 1.高阶函数
+ *  一个函数的参数是一个函数 -- 回调
+ *  一个函数 返回一个函数 -- 拆分函数
+ */
+
+/** 
+ * 函数的before
+ *  将核心逻辑提取出来，在外面增加功能
+ */
+Function.prototype.before = function (fn) {
+  return () => {
+    fn()
+    this() // 因为使用箭头函数，这里的this指向 say
+  }
+}
+
+const say = () => {
+  console.log('say...');
+}
+
+const sayBefore = say.before(() => {
+  console.log('say before ....');
+})
+
+// sayBefore()
+
+
+/** 
+ * 2.事务
+ *  开始的时候做某件事，结束的时候再做某件事
+ */
+// 期望再say之前和之后都做一些事情
+const perform = (fn, wrapperList) => {
+  // 1. 先将所有初始化的方法执行一遍
+  wrapperList.forEach(wrap => {
+    wrap.initilizae()
+  })
+
+  // 2. 执行核心方法
+  fn()
+
+  // 3. 最后将所有的结束方法执行一遍
+  wrapperList.forEach(wrap => {
+    wrap.close()
+  })
+
+}
+perform(() => {
+  console.log('say');
+}, [{ //数组中的每一个对象相当于一个 wrapper
+    initilizae() {
+      console.log('wrapper1 before....');
+    },
+    close() {
+      console.log('wrapper1 after...');
+    }
+  },
+  { //数组中的每一个对象相当于一个 wrapper
+    initilizae() {
+      console.log('wrapper2 before....');
+    },
+    close() {
+      console.log('wrapper2 after...');
+    }
+  }
+])
+
+
+/** 
+ * 3. 柯里化
+ */
+// 3.1
+const checkType = (type) => {
+  // return Object.prototype.toString.call(content).slice(8, -1) === type
+  return (content) => {
+    return Object.prototype.toString.call(content).slice(8, -1) === type
+  }
+}
+const isString = checkType('String')
+
+// console.log(checkType(1, 'Number'));
+// console.log(checkType('1', 'Number'));
+// console.log(isString(2));
+// console.log(isString('2'));
+
+
+// 3.2
+const add = (a, s, d, f, g) => {
+  return a + s + d + f + g
+}
+
+const curring = (fn, arr = []) => {
+  let len = fn.length
+  return (...args) => {
+    arr = [...arr, ...args]
+    if(arr.length < len){
+      return curring(fn, arr)
+    }
+
+    return fn(...arr)
+  }
+}
+
+console.log(curring(add)(1,2)(3)(4,5));
+
+const checkType2 = (type, content) => {
+  return Object.prototype.toString.call(content).slice(8, -1) === type
+}
+console.log(curring(checkType2)('Number')(2));
+console.log(curring(checkType2)('String')('2'));
+console.log(curring(checkType2)('String')(2));
+
+/** 
+ * 4. after
+ *  调用函数多少次之后执行回掉
+*/
+const after = (times, fn) => {
+  return () => {
+    if(--times === 0){
+      fn()
+    }
+  }
+}
+
+const newAfter =after(2, () => {
+  console.log('after action...');
+})
+
+newAfter()
+newAfter()
